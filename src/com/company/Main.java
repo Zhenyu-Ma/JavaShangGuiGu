04475c7d1553379d4a3c4d7932fe011487c7f926
MAZHENYU <528173304@qq.com>
2022-07-22 16:53:55 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world!");
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
     }
 }
