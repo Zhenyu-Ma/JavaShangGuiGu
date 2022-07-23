818f104e19a1c6aeb34edf3ac560b9542ac14940
MAZHENYU <528173304@qq.com>
2022-07-23 16:47:00 +0100

    class constant

--- src/Chapter3/Constant.java
+++ src/Chapter3/Constant.java
@@ -1,4 +1,9 @@
 package Chapter3;
 
 public class Constant {
+     static final double INTEREST = 0.03;  // 类常量 class constant
+
+    public static void main(String[] args) {
+        System.out.println(INTEREST);
+    }
 }
