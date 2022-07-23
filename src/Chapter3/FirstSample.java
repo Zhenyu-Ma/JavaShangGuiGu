8ba1b9ff07e0395c701c5e788c35b4d56cf12640
MAZHENYU <528173304@qq.com>
2022-07-23 16:41:57 +0100

    constant

--- src/Chapter3/FirstSample.java
+++ src/Chapter3/FirstSample.java
@@ -1,9 +1,19 @@
 package Chapter3;
 
+/**
+ * This is the first sample program in Core java chapter 3
+ * @version 1,01 2022-07-23
+ * @author zhenyuma
+ */
 public class FirstSample{
     public static void main(String[] args) {
-
+        System.out.println("We will not use 'Hello World!'");
+        byte b1 = 127;
+        int b2 =  0b11110001;
+        System.out.println(2.0 - 1.9);
+        System.out.println("mazhenyu\b");  //
+        System.out.println("\r");
+        System.out.println(1);
     }
-
 
 }
