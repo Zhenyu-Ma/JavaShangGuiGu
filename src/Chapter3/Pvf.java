4d9ada4271471cb25564288f4822469d2daff01a
MAZHENYU <528173304@qq.com>
2022-07-25 15:35:24 +0100

    Create Pvf.java

---
+++ src/Chapter3/Pvf.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+
+public class Pvf{
+    static boolean Paddy;
+    public static void main(String args[]){
+        System.out.println(Paddy);
+        Boolean b01 ;
+        // System.out.println(b01); 报错，
+    }
+}
